17ee19026703429b197f0bc3a874a60ee1c1129a
Luis Carlos <lcarlos.rendon@udea.edu.co>
2020-09-10 10:40:01 -0500

    menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int menu=0;
+        System.out.println("**************************************");
+        System.out.println("************Menú principal************");
+        System.out.println("**************************************");
+
+        switch(menu){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
     }
 }
