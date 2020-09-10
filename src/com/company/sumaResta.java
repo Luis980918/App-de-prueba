534556e9bb1a82358571d013d229f927c13ca2b6
DESKTOP-KFJE4B7\PC <charlstore@gmail.com>
2020-09-10 12:05:46 -0500

    Clase de suma y resta sin terminar

---
+++ src/com/company/sumaResta.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class sumaResta {
+    int a, b, resul;
+
+    public sumaResta (int num1, int num2){
+
+        this.a=num1;
+        this.b=num2;
+    }
+
+    public sumar(){
+
+
+    }
+}
